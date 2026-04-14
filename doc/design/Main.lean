352a304cbe66e0424268720ca0063e218e6d8a77
nemonuri <nemonuri2@gmail.com>
2026-04-14 09:07:10 +0900

    'Kind'와 'NamedKind' 를 분리!

--- doc/design/Main.lean
+++ doc/design/Main.lean
@@ -9,40 +9,27 @@
 
 -/
 
+inductive Kind where
+  | star : Kind
+  | arrow : Kind → Kind → Kind
 
-inductive FωType : (rank: Nat) → Type 0 where
-  | star : Nat → FωType 0
-  | arrow :
-      ( leftRank: Nat ) → ( lhs: FωType leftRank )
-    → ( rightRank: Nat ) → ( rhs: FωType rightRank )
-    → FωType (rightRank + 1)
-  | app :
-      ( funcRank: Nat ) → ( funcRank > 0 ) → ( func: FωType funcRank )
-    → ( argRank: Nat ) → ( arg: FωType argRank )
-    → FωType (funcRank - 1)
+inductive NamedKind : Kind → Type 0 where
+  | star : String → NamedKind .star
+  | arrow : (pk: Kind) → (pnk: NamedKind pk) → (qk: Kind) → (qnk: NamedKind qk) → NamedKind (.arrow pk qk)
 
+inductive CanonType : Kind → Type 0 where
+  | var : String → CanonType .star
+  | lam : (pk: Kind) → (qk: Kind) → (lct: CanonType pk) → (qct: CanonType qk) → CanonType (.arrow pk qk)
 
+inductive App (pk: Kind) (qk: Kind) (lct: CanonType (.arrow pk qk)) (pct: CanonType pk) where
+  | mk : App pk qk lct pct
 
+def eval
+  (pk: Kind) (qk: Kind) (lct: CanonType (.arrow pk qk)) (pct: CanonType pk)
+  (app: App pk qk lct pct)
+  : CanonType qk :=
+  sorry
 
-
-
-/-
-inductive FωStar where
-  | mk : Nat → FωStar
-
-mutual
-
-  inductive FωArrow where
-    | mk : FωType → FωType → FωArrow
-
-  inductive FωType where
-    | star : FωStar → FωType
-    | arrow : FωArrow → FωType
-    | app : FωArrow → FωType → FωType
-
-
-end
--/
 
 /-!
 
