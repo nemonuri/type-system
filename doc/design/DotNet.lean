eca8bab0b885d1e6b461f099e1c4f840d86ee785
nemonuri <nemonuri2@gmail.com>
2026-04-19 13:39:43 +0900

    nested inductive...이거 어떻게 우회 안 되나;;

--- doc/design/DotNet.lean
+++ doc/design/DotNet.lean
@@ -5,6 +5,11 @@ namespace DotNet
 structure TypeDef where
   name : String
   arity : Nat
-  deriving BEq, Hashable, DecidableEq
+  deriving DecidableEq
+
+
+inductive TypeSpec where
+  | var : TypeSpec
+  | app : (typeDef: TypeDef) → (args: List TypeSpec) → TypeSpec
 
 end DotNet
