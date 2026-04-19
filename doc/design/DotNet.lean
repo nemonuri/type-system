b5ce176232cff6a66b7e95aa5e7adcbef3646835
nemonuri <nemonuri2@gmail.com>
2026-04-19 19:14:28 +0900

    TypeSpec 이 이런 방법으로 정의 가능하긴 하네 - BEq, Hashable 은 어떻게 정의하지?

--- doc/design/DotNet.lean
+++ doc/design/DotNet.lean
@@ -5,11 +5,34 @@ namespace DotNet
 structure TypeDef where
   name : String
   arity : Nat
-  deriving DecidableEq
+  deriving BEq, Hashable
+
+abbrev Pos := { x : Nat // 0 < x }
+
+mutual
+  inductive TypeCon : Nat → Type 0 where
+    | init : (typeDef: TypeDef) → TypeCon typeDef.arity
+    | app : {arity: Pos} → (prev: TypeCon arity) → (typeSpec: TypeSpec) → TypeCon (arity-1)
+
+  inductive TypeSpec where
+    | var : TypeSpec
+    | con : (TypeCon 0) → TypeSpec
+
+end
 
 
-inductive TypeSpec where
-  | var : TypeSpec
-  | app : (typeDef: TypeDef) → (args: List TypeSpec) → TypeSpec
+/-
+inductive TypeSpecRaw where
+  | var : TypeSpecRaw
+  | app : (typeDef: TypeDef) → (args: List TypeSpecRaw) → TypeSpecRaw
+
+
+def TypeSpecRaw.isValid (tsr: TypeSpecRaw) : Bool :=
+  match tsr with
+  | var => .true
+  | app td args =>
+      if args.length != td.arity then .false else
+      List.all args TypeSpecRaw.isValid
+-/
 
 end DotNet
