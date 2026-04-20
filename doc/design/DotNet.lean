a00a76d0d28c085f4884fbbc4ef2f18d5a3387e7
nemonuri <nemonuri2@gmail.com>
2026-04-20 11:30:16 +0900

    inductive type은, 수많은 숨은 타입들을 만든다

--- doc/design/DotNet.lean
+++ doc/design/DotNet.lean
@@ -9,30 +9,42 @@ structure TypeDef where
 
 abbrev Pos := { x : Nat // 0 < x }
 
+instance : BEq Pos where
+  beq p1 p2 := p1.val == p2.val
+
+instance : Hashable Pos where
+  hash p := hash p.val
+
+
 mutual
   inductive TypeCon : Nat → Type 0 where
     | init : (typeDef: TypeDef) → TypeCon typeDef.arity
-    | app : {arity: Pos} → (prev: TypeCon arity) → (typeSpec: TypeSpec) → TypeCon (arity-1)
+    | app : (arity: Pos) → (prev: TypeCon arity) → (typeSpec: TypeSpec) → TypeCon (arity-1)
+    deriving Hashable
 
   inductive TypeSpec where
     | var : TypeSpec
     | con : (TypeCon 0) → TypeSpec
-
+    deriving Hashable
 end
+
+#check TypeSpec.rec
+
+#check TypeCon.rec
+#check TypeCon.casesOn
+#check TypeCon.noConfusion
+#check TypeCon.noConfusionType
+
 
 
 /-
-inductive TypeSpecRaw where
-  | var : TypeSpecRaw
-  | app : (typeDef: TypeDef) → (args: List TypeSpecRaw) → TypeSpecRaw
-
-
-def TypeSpecRaw.isValid (tsr: TypeSpecRaw) : Bool :=
-  match tsr with
-  | var => .true
-  | app td args =>
-      if args.length != td.arity then .false else
-      List.all args TypeSpecRaw.isValid
+def TypeSpec.eq (ts1 ts2: TypeSpec) : Bool :=
+  match (ts1, ts2) with
+  | (var, con _) | (con _, var) => .false
+  | (var, var) => .true
+  | (con tc1, con tc2) =>
+  match (tc1, tc2) with
+  | (TypeCon.init td1, TypeCon.init td2) => .false
 -/
 
 end DotNet
