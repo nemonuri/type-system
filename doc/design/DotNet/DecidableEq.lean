4ab0c30072497976e81734c1dd092909d2bb9e10
nemonuri <nemonuri2@gmail.com>
2026-04-24 08:39:56 +0900

    이런 멍청하긴! '0 < x' 라는 '증명' 까지 '숨김' 해 버리면 어떡해!!!

--- doc/design/DotNet/DecidableEq.lean
+++ doc/design/DotNet/DecidableEq.lean
@@ -9,16 +9,21 @@ namespace DotNet
 mutual
 
   def TypeCon.beq
-    {arity1: Nat} (typeCon1: TypeCon arity1)
-    {arity2: Nat} (typeCon2: TypeCon arity2)
+    {arity: Nat} (typeCon1 typeCon2: TypeCon arity)
     : Bool :=
     open TypeCon in
-    match typeCon1, typeCon2 with
+    let arity2' := arity
+    have lemma1 : TypeCon arity = TypeCon arity2' := by rfl
+    let typeCon2' := cast lemma1 typeCon2
+    match typeCon1, typeCon2' with
     | .init td1, .init td2 => td1 == td2
-    | .app pred1 last1, .app pred2 last2 =>
+    | .app pred1 last1, .app pred2' last2 =>
       TypeSpec.beq last1 last2 &&
-      TypeCon.beq pred1 pred2
-    | _, _ => false
+      TypeCon.beq pred1 (cast (by
+        rename_i n1 n2
+        let .mk a1 a2 := n1
+        ) pred2')
+    |  _, _ => false
 
   def TypeSpec.beq
     (typeSpec1 typeSpec2: TypeSpec)
@@ -60,6 +65,31 @@ instance {arity: Nat} : Hashable (TypeCon arity) where
 
 instance : Hashable TypeSpec where
   hash := TypeSpec.hash
+
+mutual
+
+  theorem TypeCon.hashEq
+    {arity: Nat} (typeCon1: TypeCon arity1)
+    {arity2: Nat} (typeCon2: TypeCon arity2)
+    : (typeCon1 == typeCon2) = true → hash a = hash b :=
+    open TypeCon in
+    match typeCon1, typeCon2 with
+    | .init td1, .init td2 => td1 == td2
+    | .app pred1 last1, .app pred2 last2 =>
+      TypeSpec.beq last1 last2 &&
+      TypeCon.beq pred1 pred2
+    | _, _ => false
+
+  theorem TypeSpec.hashEq
+    (typeSpec1 typeSpec2: TypeSpec)
+    : Bool :=
+    match typeSpec1, typeSpec2 with
+    | .var, .var => .true
+    | .con tc1, .con tc2 => TypeCon.beq tc1 tc2
+    | _, _ => .false
+
+
+end
 
 end DotNet
 
