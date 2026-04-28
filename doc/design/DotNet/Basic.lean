0ebc66ca0a97ed0dc7d3a15724b9648dc5c891a0
nemonuri <nemonuri2@gmail.com>
2026-04-28 18:34:44 +0900

    ....애초에, 맥락 정보가 생략된 데이터 타입을 BEq 하려던 게 잘못이었어...!!! - Dependent Type 을 사용하더라도, Decideable 하려면 상황 정보를 모두 Data로 표현할 수 있어야 한다...!

--- doc/design/DotNet/Basic.lean
+++ doc/design/DotNet/Basic.lean
@@ -1,5 +1,7 @@
 module
 
+import Mathlib.Logic.Basic
+
 public section
 
 namespace DotNet
@@ -74,32 +76,12 @@ def remainedCount (tst: Indexless) : Nat := tst.fst
 
 def toIndexed (tst: Indexless) : TypeStack tst.remainedCount := tst.snd
 
-theorem eq_imp_indexed_heq
-  {tst1 tst2: Indexless} (is_eq: tst1 = tst2)
-  : (tst1.remainedCount = tst2.remainedCount) ∧ (tst1.toIndexed ≍ tst2.toIndexed) := by
-  let ⟨_,_⟩ := tst1
-  let ⟨_,_⟩ := tst2
-  constructor
-  · injection is_eq
-  · injection is_eq
-
-/-
-theorem indexed_heq_imp_eq
-  {rc1 rc2: Nat} {tst1: TypeStack rc1} {tst2:TypeStack rc2}
-  (is_heq: tst1 ≍ tst2)
-  : (Indexless.ofIndexed tst1) = (Indexless.ofIndexed tst2) := by
-  let getRc {rc₀} (tst₀: TypeStack rc₀) := rc₀
-  have lemma1 {rc₀} (tst₀: TypeStack rc₀) : rc₀ = getRc tst₀ := by rfl
-  let asdf := is_heq.ndrecOn (lemma1 tst1)
--/
-
-
-  --have lemma1 :=
 def default (rc: Nat) : Indexless := TypeStack.default rc |> ofIndexed
 
 @[simp]
 theorem default_eq_sigma (rc: Nat) : default rc = Sigma.mk rc (TypeStack.default rc) := by rfl
 
+@[simp]
 theorem default_eq_imp_rc_eq
   {rc1 rc2: Nat}
   (default_eq: (default rc1) = (default rc2))
@@ -107,14 +89,38 @@ theorem default_eq_imp_rc_eq
   simp at default_eq
   exact default_eq.left
 
-
-
+theorem default_congr (rc: Nat)
+  : (default rc).toIndexed ≍ (TypeStack.default rc) := by
+  rfl
 
 end Indexless
 
+/-
+theorem default_heq_imp_rc_eq
+  {rc1 rc2: Nat}
+  (default_eq: (default rc1) ≍ (default rc2))
+  : rc1 = rc2 := by
+  induction rc1 with
+  | zero =>
+    induction rc2 with
+    | zero => rfl
+    | succ n₁_₂ ih₁_₂ =>
+      unfold default cast Pos.mk Decidable.byCases at default_eq
+      simp at default_eq
+      unfold Nat.decLt Nat.decLe at default_eq
+      simp at default_eq
+      unfold Api.System_Tuple Api.System_Tuple' at default_eq
+      simp at default_eq
+-/
 
-set_option pp.proofs true in
-#print HEq.rec
+
+
+
+
+
+
+
+
 
 
 
@@ -176,6 +182,18 @@ def ofIndex (rc: Nat) : TypeStack rc := Inhabited.default
 
 theorem ofIndex_toIndex (rc: Nat) : (ofIndex rc).toIndex = rc := by rfl
 
+def remainedCount {rc: Nat} (tst: TypeStack rc) : { x : Nat // x = rc } :=
+  match tst with
+  | .alloc td =>
+    Subtype.mk td.arity (by rfl)
+  | .push rc₂ _ _ =>
+    Subtype.mk (rc₂.val - 1) (by rfl)
+
+theorem remainedCount_typeStack_eq {rc: Nat} (tst: TypeStack rc) : rc = tst.remainedCount.val := by
+  unfold remainedCount
+  split
+  next => simp
+  next => simp
 
 @[expose]
 abbrev pushAuto {rc: Pos} (tst: TypeStack rc) (tsp: TypeSpec) : TypeStack (rc.val - 1) :=
@@ -194,15 +212,100 @@ theorem typeDef_is_invariant {rc: Pos} (tst: TypeStack rc) (tsp: TypeSpec)
 def capacity {rc: Nat} (tst: TypeStack rc) : Nat := tst.typeDef.arity
 
 
-#print TypeStack.noConfusion
+  --conv at lemma4 =>
+  --  lhs
+  --have lemma5 := HEq.subst is_heq lemma4
+  --have lemma5 : tst1.remainedCount.val = tst2.remainedCount.val := by
+  --conv at lemma4 =>
+    --lhs
 
-theorem type_eq_imp_rc_eq {rc1 rc2: Nat} (type_eq: TypeStack rc1 = TypeStack rc2) : rc1 = rc2 := by
+
+/-
+  have lemma_eq1 {rc₀} (tst₀: TypeStack rc₀) := tst₀.remainedCount_typeStack_eq
+  rw [lemma_eq1 tst1, lemma_eq1 tst2]
+  let lemma_eq2 {rc₀} (tst₀: TypeStack rc₀) := lemma_eq1 tst₀ |> congrArg TypeStack
+  let cast_self {rc₀} (tst₀: TypeStack rc₀) := tst₀ |> cast (lemma_eq2 tst₀)
+  have lemma_cast_self_heq {rc₀} (tst₀: TypeStack rc₀) : tst₀ ≍ cast_self tst₀ := by
+    unfold cast_self
+    have lemma1 := cast_heq (lemma_eq2 tst₀) tst₀
+    exact lemma1.symm
+  have lemma1 : cast_self tst1 ≍ cast_self tst2 :=
+    is_heq.trans (lemma_cast_self_heq tst2) |> HEq.trans (lemma_cast_self_heq tst1).symm
+  --obtain ⟨v1, p1⟩ := tst1.remainedCount
+  obtain ⟨v2, p2⟩ := tst2.remainedCount
+  have type_eq := type_eq_of_heq lemma1
+-/
+
+
+/-
+  let tsrc1 := TypeStack.remainedCount (rc := rc1)
+  let tsrc2 := TypeStack.remainedCount (rc := rc2)
+  have lemma_trsc_heq : tsrc1 ≍ tsrc2 := by
+    unfold tsrc1 tsrc2
+    unfold TypeStack.remainedCount
+-/
+  --have lemma1 (rc_heq: tst1.remainedCount ≍ tst2.remainedCount) : (tst1.remainedCount.val = rc1) ↔ (tst2.remainedCount.val = rc2) := by
+  --  subst_eqs
+
+
+  --suffices lemma1 : (tst1.remainedCount.val = rc1) ↔ (tst2.remainedCount.val = rc2) from by
+    --obtain ⟨mp, mpr⟩ := lemma1
+    --have lemma2 := is_heq.elim tst1.remainedCount.property
+/-
+    unfold remainedCount at lemma1
+    split at lemma1
+    next =>
+      simp at lemma1
+      split at lemma1
+      next =>
+
+
+
+
+
+      next => simp at lemma1; exact lemma1
+    next =>
+      simp at lemma1
+      split at lemma1
+      next => simp at lemma1; exact lemma1
+      next => simp at lemma1; exact lemma1
+-/
+  --congr
+
+set_option pp.proofs true in
+#print HEq.rec
+
+theorem teststset
+  (tycon: Nat → Type 0)
+  (n1 n2: Nat)
+  (inst1: tycon n1) (inst2: tycon n2)
+  --(toIndex: (n: Nat) → (inst: tycon n) → Nat)
+  --(toIndex_is_bij: ∀ (n: Nat) (inst: tycon n), toIndex n inst = n)
+  (inst_heq: inst1 ≍ inst2)
+  : (n1 = n2) :=
+  let heq_α :=
+
+
+
+theorem type_eq_imp_rc_eq {rc1 rc2: Nat} (type_eq: TypeStack rc1 = TypeStack rc2) : (rc1 = rc2) := by
+  let getRc {rc₀: Nat} (tst₀: TypeStack rc₀) : Nat := rc₀
+  sorry
+
+  --let invTyApp {rc₀} (t₀: { x: Type 0 // x = TypeStack rc₀ }) := rc₀
+  --have lemma1 := congrArg invTyApp type_eq
+
+
+
+
+/-
   have lemma1 := ofIndex_toIndex rc1
   have lemma2 := ofIndex_toIndex rc2
   unfold TypeStack.ofIndex at lemma1 lemma2
+-/
 
 
 
+-- Note: (tst: TypeStack rc) 가 Data로서 존재한다는 명제만으로는, (rc: Nat) 이 Data로서 존재한다는 명제를 증명할 수 없다!
 
 
 
@@ -406,6 +509,49 @@ def toVector (typeStack: Initialized) : Vector TypeSpec (typeStack.capacity) :=
   cast lemma2 result
 
 end Initialized
+
+set_option pp.proofs true in
+#print HEq.rec
+
+
+
+--set_option pp.proofs true in
+set_option pp.proofs true in
+theorem heq_imp_rc_eq
+  {rc1 rc2: Nat} {tst1: TypeStack rc1} {tst2: TypeStack rc2}
+  (is_heq: tst1 ≍ tst2)
+  : rc1 = rc2 := by
+  let getRc {rc₀: Nat} (tst₀: TypeStack rc₀) := rc₀
+  have ht (rc1₀ rc2₀: Nat) (heq₀: rc1₀ ≍ rc2₀) : id rc1₀ = id rc2₀ := by simp_all
+  sorry
+
+/-
+  match tst1 with
+  | .alloc td₁ =>
+    match tst2 with
+    | .alloc td₂ =>
+      have lemma1 := dcongr_heq
+      rewrite [cas] at is_heq
+
+  have lemma1 := ((type_eq_of_heq is_heq |> cast_heq <| tst1) |> HEq.trans <| is_heq) |> eq_of_heq
+  let mtv {rc₀} (tst: TypeStack rc₀) := tst.remainedCount.val
+  have lemma2 : mtv tst2 = mtv tst1 := by --congrArg mtv lemma1
+    calc
+      mtv tst2 = _ := congrArg mtv lemma1 |> Eq.symm
+      _ = mtv tst1 := by
+        unfold mtv TypeStack.remainedCount Subtype.val
+        split
+        next td₁ _ =>
+          simp
+          split
+          next td₂ _ =>
+            simp_all
+            have lemma2 := heq_of_heq_of_eq is_heq lemma1.symm
+            have mtv {rc₀} (tst1₀ tst2₀: TypeStack rc₀) : tst1₀.capacity = tst2₀.capacity := by
+              simp only [capacity_eq_remainedCount_plus_length]
+            have lemma3 :=
+              lemma2.rec
+-/
 
 end TypeStack
 
