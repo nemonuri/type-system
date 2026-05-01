f2b570e48fccfd0233e2d456928e6c3f6363a594
nemonuri <nemonuri2@gmail.com>
2026-05-01 12:09:35 +0900

    먼저, 타입 시스템을 제대로 공부하자!

---
+++ doc/design/FOmega/Basic.lean
@@ -0,0 +1,32 @@
+module
+
+public import DotNet
+
+public section
+
+open DotNet
+
+namespace FOmega
+
+/-!
+
+# Design
+
+## Goal
+
+1. Extend .NET type system (System F) to System Fω
+2. Enable consumers to define and apply 'Higher-Kinded' type.
+
+-/
+
+end FOmega
+
+end --public
+
+/-!
+
+## Reference
+
+- [Typechecker Zoo](https://sdiehl.github.io/typechecker-zoo/system-f-omega/language-design.html)
+
+-/
